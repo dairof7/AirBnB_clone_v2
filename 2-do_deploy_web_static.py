3225293bbe6b986f572c0ec21c66affa23bc6d51
dairof7 <dairof7@gmail.com>
2020-08-17 20:14:36 -0500

    move the verify code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,24 +19,17 @@ def do_deploy(archive_path):
     path_no_ext = _path[1].split('.')[0]
 
     try:
-        # upload file to /tmp/ on webservers
         put(archive_path, '/tmp')
-        # create dir
         run("sudo mkdir -p /data/web_static/releases/" + path_no_ext + '/')
-        # uncompress file in
         run("sudo tar -xzf /tmp/" + path_with_ext +
             ' -C /data/web_static/releases/'
             + path_no_ext + '/')
-        # Delete the archive from the web server
         run("sudo rm /tmp/" + path_with_ext)
         run("sudo mv /data/web_static/releases/" + path_no_ext +
             "/web_static/* /data/web_static/releases/" + path_no_ext + "/")
-        # Move files
         run("sudo rm -rf /data/web_static/releases/" +
             path_no_ext + "/web_static")
-        # Delete the symbolic link
         run("sudo rm -rf /data/web_static/current")
-        # Create
         run("sudo ln -s /data/web_static/releases/" + path_no_ext +
             "/ /data/web_static/current")
 
