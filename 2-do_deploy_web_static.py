372bc1e29bd30b3d31ee505009822defea45f9f0
dairof7 <dairof7@gmail.com>
2020-08-17 20:00:42 -0500

    fix alot of errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,71 +1,45 @@
 #!/usr/bin/python3
-# Distributes an archive to your web servers
+"""Generates a .tgz archive"""
+
+from fabric.api import run, put, env
+from os.path import exists
 
 
-from datetime import datetime
-from fabric.api import env, local, put, run
-from os import path
-
-
-env.hosts = ['34.228.37.117', '54.234.227.98']
+env.hosts = ['35.227.29.60', '54.196.131.110']
 env.user = 'ubuntu'
 
 
-def do_pack():
-    """
-    folder to .tgz
-    """
-    mydate = datetime.now()
-    myfile = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
-        mydate.year,
-        mydate.month,
-        mydate.day,
-        mydate.hour,
-        mydate.minute,
-        mydate.second
-    )
-    local('mkdir -p versions')
-    answer = local('tar -cvzf ' + myfile + ' web_static')
-    if answer.succeeded:
-        return myfile
-    return None
-
-
 def do_deploy(archive_path):
-    """Deploy archive to web servers"""
-    if not path.exists(archive_path) and not path.isfile(archive_path):
-        return False
+    """Distributes an archive to your web servers."""
+    _path = archive_path.split('/')
+    path_with_ext = _path[1]
+    path_no_ext = _path[1].split('.')[0]
 
-    myspath = archive_path.split('/')[1].split(".")
-    i = myspath[0]
+    if not exists(archive_path):
+        return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
+        # upload file to /tmp/ on webservers
         put(archive_path, '/tmp')
-
         # create dir
-        run("sudo mkdir -p /data/web_static/releases/" + i + "/")
-
-        # Uncompress the archive to the folder
-        # /data/web_static/releases/<archive filename
-        # without extension> on the web server
-        run("sudo tar -xzf /tmp/" + i + ".tgz" +
-            " -C /data/web_static/releases/" + i + "/")
-
+        run("sudo mkdir -p /data/web_static/releases/" + path_no_ext + '/')
+        # uncompress file in
+        run("sudo tar -xzf /tmp/" + path_with_ext +
+            ' -C /data/web_static/releases/'
+            + path_no_ext + '/')
         # Delete the archive from the web server
-        run("sudo rm /tmp/" + i + ".tgz")
-
-        run("sudo mv /data/web_static/releases/" + i +
-            "/web_static/* /data/web_static/releases/" + i + "/")
-
+        run("sudo rm /tmp/" + path_with_ext)
+        run("sudo mv /data/web_static/releases/" + path_no_ext +
+            "/web_static/* /data/web_static/releases/" + path_no_ext + "/")
+        # Move files
+        run("sudo rm -rf /data/web_static/releases/" +
+            path_no_ext + "/web_static")
         # Delete the symbolic link
-        run("sudo rm -rf /data/web_static/releases/" + i + "/web_static")
         run("sudo rm -rf /data/web_static/current")
-
-        # Create a new the symbolic link
-        run("sudo ln -s /data/web_static/releases/" + i +
+        # Create
+        run("sudo ln -s /data/web_static/releases/" + path_no_ext +
             "/ /data/web_static/current")
+
+        return True
     except Exception:
         return False
-
-    return True
