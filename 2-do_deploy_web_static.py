8137649356c073de97df1320aed69af420903a4e
dairof7 <dairof7@gmail.com>
2020-08-17 20:25:56 -0500

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-"""update - uncompress and create simbolic link"""
+"""Generates a .tgz archive"""
 
 from fabric.api import run, put, env
 from os.path import exists
 
 env.user = 'ubuntu'
-env.hosts = ['35.227.29.60', '54.196.131.110']
+env.hosts = ['35.237.242.122', '54.234.135.146']
 
 
 def do_deploy(archive_path):
     """
-        Distributes an archive to your web servers
+        deploy the archive to the webservers
     """
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
 
-    _path = archive_path.split('/')
-    path_with_ext = _path[1]
-    path_no_ext = _path[1].split('.')[0]
+    filename_wo_ext = archive_path[9:34]
+    filename_w_ext = archive_path[9:]
+    input_path = "/data/web_static/releases/{}/".format(filename_wo_ext)
 
     try:
-        put(archive_path, '/tmp')
-        run("sudo mkdir -p /data/web_static/releases/" + path_no_ext + '/')
-        run("sudo tar -xzf /tmp/" + path_with_ext +
-            ' -C /data/web_static/releases/'
-            + path_no_ext + '/')
-        run("sudo rm /tmp/" + path_with_ext)
-        run("sudo mv /data/web_static/releases/" + path_no_ext +
-            "/web_static/* /data/web_static/releases/" + path_no_ext + "/")
-        run("sudo rm -rf /data/web_static/releases/" +
-            path_no_ext + "/web_static")
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/" + path_no_ext +
-            "/ /data/web_static/current")
-
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(input_path))
+        run("sudo tar -zxvf /tmp/{} -C {}".format(filename_w_ext, input_path))
+        run("sudo rm -rf /tmp/{}".format(filename_w_ext))
+        run("sudo mv -n {}/web_static/* {}".format(input_path, input_path))
+        run("sudo rm -rf {}/web_static".format(input_path))
+        run("sudo rm /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(input_path))
         return True
+
     except Exception:
         return False
