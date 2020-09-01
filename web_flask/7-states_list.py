4f87a4df5ef69f4490121bb11ff35784a516714c
dairof7 <dairof7@gmail.com>
2020-09-01 15:34:51 -0500

    pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,19 +9,18 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_db(self):
     """Close session"""
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def stateList():
     """function to execute"""
     states = storage.all(State).values()
-    return render_template('7-states_list.html', states = states)
-
-
-
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
